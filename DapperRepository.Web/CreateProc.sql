c9d4992905cbc3ec4e287a450e3b2d6d6b700faa
Olek-HZQ <8757558798@qq.com>
2019-03-03 13:58:27 +0800

    Optimize stored procedures named 'DRD_Customer_GetAllCustomers'

--- DapperRepository.Web/CreateProc.sql
+++ DapperRepository.Web/CreateProc.sql
@@ -1,14 +1,12 @@
-USE [DapperRepositoryDb]
+USE [DapperSampleDb];
 GO
 
-/****** Object:  StoredProcedure [dbo].[DRD_Customer_GetAllCustomers]    Script Date: 3/2/2019 4:14:56 PM ******/
-SET ANSI_NULLS ON
+/****** Object:  StoredProcedure [dbo].[DRD_Customer_GetAllCustomers]    Script Date: 3/3/2019 1:23:45 PM ******/
+SET ANSI_NULLS ON;
 GO
 
-SET QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER ON;
 GO
-
-
 
 CREATE PROCEDURE [dbo].[DRD_Customer_GetAllCustomers]
     (
@@ -28,48 +26,37 @@ AS
         SET @PageLowerBound = @PageSize * @PageIndex;
         SET @PageUpperBound = @PageLowerBound + @PageSize + 1;
 
-        CREATE TABLE #DisplayOrderTmp
-            (
-              Id INT IDENTITY(1, 1)
-                     NOT NULL ,
-              [CustomerId] INT NOT NULL
-            );
-
-        
-        INSERT  INTO #DisplayOrderTmp
-                ( CustomerId
-                )
-                SELECT  Id
-                FROM    dbo.Customer
-                ORDER BY Id DESC;
-       
-
         CREATE TABLE #PageIndex
             (
               [IndexId] INT IDENTITY(1, 1)
                             NOT NULL ,
               [CustomerId] INT NOT NULL
             );
+
         INSERT  INTO #PageIndex
                 ( CustomerId
                 )
-                SELECT  CustomerId
-                FROM    #DisplayOrderTmp
-                GROUP BY CustomerId
-                ORDER BY MIN(Id);
+                SELECT  Id
+                FROM    dbo.Customer
+                ORDER BY Id DESC;
 
-				-- total records
+		-- total records
         SET @TotalRecords = @@ROWCOUNT;
 
-        DROP TABLE #DisplayOrderTmp;
-
-				-- return customers
+		-- return customers
         SELECT TOP ( @RowsToReturn )
-                c.Id,c.Username,c.Email,c.Active,c.CreationTime,cr.Id,cr.Name,cr.SystemName
+                c.Id ,
+                c.Username ,
+                c.Email ,
+                c.Active ,
+                c.CreationTime ,
+                cr.Id ,
+                cr.Name ,
+                cr.SystemName
         FROM    #PageIndex [pi]
                 INNER JOIN dbo.Customer c ON c.Id = [pi].CustomerId
-				INNER JOIN Customer_CustomerRole_Mapping crm ON c.Id = crm.CustomerId
-				INNER JOIN CustomerRole cr ON crm.CustomerRoleId = cr.Id
+                INNER JOIN Customer_CustomerRole_Mapping crm ON c.Id = crm.CustomerId
+                INNER JOIN CustomerRole cr ON crm.CustomerRoleId = cr.Id
         WHERE   pi.IndexId > @PageLowerBound
                 AND pi.IndexId < @PageUpperBound
         ORDER BY pi.IndexId;
@@ -77,7 +64,3 @@ AS
         DROP TABLE #PageIndex;
 
     END;
-
-GO
-
-
